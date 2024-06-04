772f1143fdede9dedc5a8bc37031021a4e54b0fe
Matthew Madden <matthewkayin@gmail.com>
2024-06-04 17:23:19 -0500

    Add BrowserRouter
    Signed-off-by: Matthew Madden <matthewkayin@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './styles/global.css';
 import { ProductPage } from './pages';
 import { Banner } from './components';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <Banner />
-    <ProductPage />
+    <BrowserRouter>
+      <Banner />
+      <ProductPage />
+    </BrowserRouter>
   </React.StrictMode>
 );
